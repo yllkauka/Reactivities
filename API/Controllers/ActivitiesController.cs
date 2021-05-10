02a7b0f33fcd28d90e9d5e342efbab222559ae24
Yllka Uka <yu47769@ubt-uni.net>
2021-05-11 01:49:12 +0200

    Section - 4

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -3,29 +3,43 @@ using System.Threading.Tasks;
 using System;
 using Domain;
 using Microsoft.AspNetCore.Mvc;
-using Persistence;
-using Microsoft.EntityFrameworkCore;
+using Application.Activities;
 
 namespace API.Controllers
 {
     public class ActivitiesController : BaseApiController
     {
-        private readonly DataContext _context;
-        public ActivitiesController(DataContext context)
-        {
-            _context = context;
-        }
+     
 
         [HttpGet]
         public async Task<ActionResult<List<Activity>>> GetActivities()
         {
-            return await _context.Activities.ToListAsync();
+            return await Mediator.Send(new List.Query());
         }
 
         [HttpGet("{id}")]  // activities/id
-        public async Task<ActionResult<Activity>> GetActivity(Guid id) 
+        public async Task<ActionResult<Activity>> GetActivity(Guid id)
         {
-            return await _context.Activities.FindAsync(id);
+            return await Mediator.Send(new Details.Query{Id = id});  //nese activity nuk ekziston na kthen null prej Mediator(null nuk eshte error, po kthen no content)
         }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateActivity(Activity activity) //kthen akces ne Http response types si: ok, return bad request, return not found...
+        {
+            return Ok (await Mediator.Send(new Create.Command {Activity = activity}));
+        }
+
+         [HttpPut("{id}")]
+        public async Task<IActionResult> EditActivity(Guid id, Activity activity)
+        {
+            activity.Id = id;
+            return Ok(await Mediator.Send(new Edit.Command{Activity = activity}));  //me update veq ni single field "Id" ne kete rast
+        } 
+
+         [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteActivity(Guid id)
+        {
+            return Ok(await Mediator.Send(new Delete.Command{Id = id}));
+        } 
     }
 }
